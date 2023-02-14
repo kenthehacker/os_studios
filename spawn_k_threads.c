283a111f8a8aea0a3516ed09d9f96af204fa291f
Kenichi Matsuo <kenichimatsuo@kenichis-mbp.dhcp.wustl.edu>
2023-02-14 00:16:49 -0600

    asdf

---
+++ spawn_k_threads.c
@@ -0,0 +1,67 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
+#include <linux/hrtimer.h>
+#include <linux/kthread.h>
+
+static struct task_struct *k_threads[4];
+
+int ret_zero(void *){
+    while(!kthread_should_stop()){
+        //do nothing
+    }
+    return 0;
+}
+
+static int thread_init(void){
+    printk(KERN_ALERT "k thread module loaded \n");
+    k_threads[0] = kthread_create(ret_zero, NULL, "a");
+    k_threads[1] = kthread_create(ret_zero, NULL, "b");
+    k_threads[2] = kthread_create(ret_zero, NULL, "c");
+    k_threads[3] = kthread_create(ret_zero, NULL, "d");
+
+    int i;
+    for(i = 0; i<4; i++){
+        kthread_bind(k_threads[i], i);
+        wake_up_process(k_threads[i]);
+    }
+    return 0;
+}
+
+static void thread_exit(void){
+    printk(KERN_ALERT "k thread module offloaded \n");
+    int i;
+    for(i = 0; i<4; i++){
+        kthread_stop(k_threads[i]);
+    }
+
+}
+
+module_init(thread_init);
+module_exit(thread_exit);
+MODULE_AUTHOR("kenthehacker");
+MODULE_DESCRIPTION("Thread Module");
+MODULE_LICENSE("GPL"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
