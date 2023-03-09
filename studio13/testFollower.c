5f7a417e64a294d2642ade1a5d2ea2366323a967
kenthehacker <ken.m@wustl.edu>
2023-03-09 17:59:11 -0600

    asdf

--- studio13/testFollower.c
+++ studio13/testFollower.c
@@ -12,6 +12,9 @@ int main(){
     fd = shm_open("/shared_memory_region", O_RDWR, S_IRWXU);
     my_struct = mmap(NULL, sizeof(struct shared_data), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     printf("Shared data values:\n");
+    while(my_struct->write_guard == 0){
+        
+    }
     for (int i = 0; i < shared_mem_size; i++) {
         printf("%d\n", my_struct->data[i]);
     }
