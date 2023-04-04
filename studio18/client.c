f70cefd7f31458894d56c05e8c62b15575151a26
kenthehacker <ken.m@wustl.edu>
2023-04-03 23:20:56 -0500

    sdf

--- studio18/client.c
+++ studio18/client.c
@@ -6,13 +6,16 @@
 #include <unistd.h>
 #define BUF_SIZE 1024
 
-int main(void){
+int main(int argc, char * argv[]){
     struct sockaddr_un sock_address;
     int cli_socket;
     unsigned int i;
     char buf[BUF_SIZE];
+    char user_input[100];
+    unsigned int TERMINATION_VALUE = 418;
 	const char *SOCKNAME = "PLANET_EXPRESS";
 
+    
     cli_socket = socket(AF_LOCAL, SOCK_STREAM, 0);
     if (cli_socket == -1){
        //errExit("CLIENT Socket Fail");
@@ -26,7 +29,21 @@ int main(void){
         perror("CLIENT connect fail");
 		exit(1);
     }
-    for (i = 1; i<=15; i++){
+
+    if (argc == 2 && strcmp(argv[1], "quit") == 0) {
+        printf("quitting program...\n");
+        sprintf(buf, "%u", TERMINATION_VALUE);
+        if (write(cli_socket, buf, sizeof(unsigned int)) < 0 ){
+            perror("CLIENT write Fail");
+			exit(1);
+        }
+        close(cli_socket);
+        return 0;
+    }
+
+
+    printf("sending values\n");
+    for (i = 1; i<=10; i++){
         sprintf(buf, "%u", i);
         if (write(cli_socket, buf, sizeof(unsigned int)) < 0 ){
             perror("CLIENT write Fail");
@@ -34,6 +51,29 @@ int main(void){
         }
         //utoa(i, buf, )
     }
+    /*
+    while(1==1){
+        printf("Enter some an int or 'quit' to exit: ");
+        scanf("%s", &user_input);
+        if (strcmp(user_input, "quit") == 0){
+            sprintf(buf,"%u",418);
+            if (write(cli_socket, buf, sizeof(unsigned int)) < 0){
+                perror("Client failed to kill server");
+                exit(1);
+            }
+            break;
+        }else{
+            i = (unsigned int) strtoul(user_input, NULL, 10);
+            sprintf(buf, "%u", i);
+            if (write(cli_socket, buf, sizeof(unsigned int)) < 0 ){
+                perror("CLIENT write Fail");
+			    exit(1);
+            }
+        }
+    }
+    */
+
+    printf("this client is ded\n");
     close(cli_socket);
     return 0;
 }
--- studio18/server.c
+++ studio18/server.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #define BACKLOG 5
 #define BUF_SIZE 1024
-
+#define TERMINATION_VALUE 418
 int main(void){
     int server_socket, comm_socket;
     unsigned int current_value;
@@ -15,6 +15,7 @@ int main(void){
     char buf[BUF_SIZE];
     int num_bytes;
     int byte_count;
+    int cont = 1;
 
     server_socket = socket(AF_LOCAL, SOCK_STREAM, 0);
     if (server_socket == -1){
@@ -33,12 +34,14 @@ int main(void){
         perror("listen");exit(1);
     }
 
-    comm_socket = accept(server_socket, NULL, NULL);
-    if (comm_socket == -1){
-        perror("Accept failed");exit(1);
-    }
-
-    for(;;){
+    
+    
+    while(1==1 && cont == 1){
+        comm_socket = accept(server_socket, NULL, NULL);
+        if (comm_socket == -1){
+            perror("Accept failed");
+            exit(1);
+        }    
         num_bytes = read(comm_socket, buf, sizeof(buf));
         if (num_bytes<=0){
             printf("done reading \n");
@@ -46,13 +49,17 @@ int main(void){
         }
         byte_count = 0;
         while(byte_count < num_bytes){
-            sscanf(&buf[byte_count], "%u\n", &current_value);
+            sscanf(&buf[byte_count], "%u ", &current_value);
             printf("Received value %u \n", current_value);
+            if (current_value == 418){
+                cont = 0;
+                break;
+            }
             byte_count += sizeof(unsigned int);
         }
     }
 
-    printf("cli disconnect\n");
+    printf("client(s) disconnect TERMINAL VALUE %d received\n",TERMINATION_VALUE);
     close(comm_socket);
     unlink(SOCKNAME);
     
