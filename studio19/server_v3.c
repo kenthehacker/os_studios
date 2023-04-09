ef856cfc8952718c4be088cad6665c2bee75285c
kenthehacker <ken.m@wustl.edu>
2023-04-08 22:25:49 -0500

    asdf

--- studio19/server_v3.c
+++ studio19/server_v3.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <sys/time.h>
@@ -13,89 +14,89 @@
 #define BACKLOG 5
 #define BUF_SIZE 1024
 
-int main(void){
-    int server_sock, incoming_socket;
-    char hostname[BUF_SIZE];
-    gethostname(hostname,BUF_SIZE);
+int main(int argc, char *argv[]){
+    struct sockaddr_in addr, cli_addr;
+    unsigned int end_of_message = 418;
+    int sfd, cfd;
     int port_num = 30303;
-    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed to make\n");
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1){
+        perror("server socket failed");
         exit(1);
     }
-    printf("INITIAL server_sock: %d\n",server_sock);
-    struct sockaddr_in server_addr, cli_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port_num);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_sock, (struct sockaddr *) & server_addr, sizeof(server_addr)) == -1){
-        perror("bind");exit(1);
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port_num);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    while(bind(sfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) == -1){
+        printf("bind fail, addy in use\n");
+        printf("trying to bind again...");
     }
-    if (listen(server_sock, BACKLOG) == -1){
-        perror("listen");exit(1);
+    if (listen(sfd, BACKLOG) == -1){
+        perror("listen fail");
+        exit(1);
     }
-    int max_fd = server_sock;
-    fd_set read_fds;
-    FD_ZERO(&read_fds);
-    FD_SET(server_sock, &read_fds);
-    while(1==1){
-        // printf("maxfd: %d\n",max_fd);
-        // printf("server_sock: %d\n",server_sock);
-        fd_set tmp_fds = read_fds;
-        int num_fds = select(max_fd+1, &tmp_fds, NULL, NULL, NULL);
-        if (num_fds == -1){
-            perror("select");
-            exit(-1);
-        }
-        for (int fd = 0; fd <= max_fd; fd++){
-            if (FD_ISSET(fd, &tmp_fds)){
-                if (fd == server_sock){ 
-                    socklen_t cli_addr_len = sizeof(cli_addr);
-                    incoming_socket = accept(server_sock, (struct sockaddr *) &cli_addr, &cli_addr_len);
-                    if (incoming_socket == -1){
-                        perror("accept failed");
-                        exit(-1);
-                    }
-                    else {
-                        FD_SET(incoming_socket, &read_fds);
-                        max_fd = incoming_socket > max_fd? incoming_socket : max_fd;
-                        printf("new connection established\n");
+    int nfd = sfd > STDIN_FILENO ? sfd : STDIN_FILENO;
+    fd_set readfds, writefds;
+    FD_ZERO(&readfds);
+    FD_ZERO(&writefds);
+    FD_SET(sfd, &readfds);
+    for(;;){
+        // printf("before\n");
+        fd_set temp_read_fds = readfds;
+        select(nfd+1, &temp_read_fds, &writefds, NULL, NULL);
+        // printf("after\n");
+        
+        for(int i_fd = 0; i_fd<nfd+1; i_fd++){
+            // printf("point a\n");
+            if (FD_ISSET(i_fd, &temp_read_fds)){
+                // printf("point b\n");
+                if (i_fd == sfd){
+                    // printf("point c\n");
+                    socklen_t addrlen = sizeof(cli_addr);
+                    cfd = accept(sfd, (struct sockaddr *)&cli_addr, &addrlen);
+                    FD_SET(cfd, &readfds);
+                    if (cfd>nfd){
+                        nfd = cfd;
                     }
                 }
-                else {
-                    // printf("fd: %d\n",fd);
-                    uint32_t msg;
-                    int num_bytes = read(fd, &msg, sizeof(msg));
-                    if (num_bytes == -1){
-                        perror("read");
-                        exit(-1);
+                else if (i_fd == STDIN_FILENO){
+                    char words[1024];
+                    fgets(words, 1024, stdin);
+                    if(strcmp(words,"quit\n") == 0){
+                        printf("killed program \n");
+                        return 0;
                     }
-                    else if (num_bytes == 0){ 
-                        printf("Connection closed\n");
-                        close(fd);
-                        FD_CLR(fd, &read_fds);
-                    }
-                    else { 
-                        printf("received %u \n",ntohl(msg));
-                        if (ntohl(msg) == 10) {
-                            char server_msg[BUF_SIZE];
-                            sprintf(server_msg,"%s to cli: received your digits",hostname);
-                            int write_byte_len = write(fd, server_msg, sizeof(server_msg)); //strlen?
-                            if (write_byte_len == -1){
-                                perror("write");
-                                exit(-1);
-                            }
-                            else {
-                                printf("Wrote %d bytes to cli\n",write_byte_len);
-                            }
+                    printf("read %s\n",words);
+                }else{
+                    uint32_t cli_msg;
+                    // printf("point d\n");
+                    int byte_count = read(i_fd, &cli_msg, sizeof(uint32_t));
+                    // printf("point e\n");
+                    if (byte_count == 0){
+                        char client_ip[INET_ADDRSTRLEN];
+                        inet_ntop(AF_INET, &(cli_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+                        printf("connection to %s client killed\n",client_ip);
+                        close(i_fd);
+                        FD_CLR(i_fd, &readfds);
+                    }else{
+                        unsigned int unsigned_cli_message = ntohl(cli_msg);
+                        if (unsigned_cli_message != end_of_message){
+                            printf("msg from client: %u\n",unsigned_cli_message);
                         }
+                        if(unsigned_cli_message == end_of_message){
+                            // printf("here\n");
+                            char server_to_cli_msg[BUF_SIZE];
+                            char hostname[BUF_SIZE];
+                            gethostname(hostname,BUF_SIZE);
+                            sprintf(server_to_cli_msg, "%s: end of msg", hostname);
+                            write(nfd, server_to_cli_msg, sizeof(server_to_cli_msg));
+                        }
+                        // printf("point f\n");
                     }
                 }
             }
         }
     }
-    return 0;
+    
 }
-
-
-
